b4632938735244b6a4df54ffc80e6e4489cfc210
zahrizhalali29 <zzahrizhal.ali@gmail.com>
2021-07-16 18:43:10 +0800

    Detect colision with tail

--- main.py
+++ main.py
@@ -30,6 +30,12 @@ while is_game_on:
         scoreboard.game_over()
         is_game_on = False
 
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
 
     screen.onkey(snake.up, "Up")
     screen.onkey(snake.down, "Down")
