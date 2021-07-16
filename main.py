bd5d26d316b0ddf427c7ab8f8e3a546f9d3acf03
zahrizhalali29 <zzahrizhal.ali@gmail.com>
2021-07-16 17:25:39 +0800

    Add Food Functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(600, 600)
@@ -11,12 +12,14 @@ screen.tracer(0)
 screen.listen()
 
 snake = Snake()
-
+food = Food()
 is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
     screen.onkey(snake.up, "Up")
     screen.onkey(snake.down, "Down")
     screen.onkey(snake.left, "Left")
